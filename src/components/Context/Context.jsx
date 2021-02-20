8291bd6721ee466542ae4797d423c94a7e35ba92
Vladimir <wowchik.shadrin@yandex.ru>
2021-02-20 18:49:32 +0300

    description +img

--- src/components/Context/Context.jsx
+++ src/components/Context/Context.jsx
@@ -5,7 +5,7 @@ export class Context extends Component {
   render() {
     return (
       <div className="mainBody">
-        <p>Поиск украденных велосипедов.</p>
+        <h2>Поиск украденных велосипедов.</h2>
         <img src={image} alt="logo" className="brand-logo" />
       </div>
     );
